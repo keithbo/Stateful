a10a104b6229faedb8f1c880d4d3d7304b865c24
keithbo <keith.bosacker@gmail.com>
2019-07-01 11:44:01 -0500

    Move constants to a static class

---
+++ Stateful.ServiceFabric.Actors/Constants.cs
@@ -0,0 +1,10 @@
+﻿namespace Stateful.ServiceFabric.Actors
+{
+    public static class Constants
+    {
+        public const string CollectionIndexKeyFormat = "{0}:{1:X}";
+        public const string DictionaryBucketIndexFormat = "{0}:{1:X}";
+        public const string DictionaryKeyIndexFormat = "{0}:{1:X}:{2:X}";
+        public const string DictionaryValueIndexFormat = "{0}:{1:X}:{2:X}:v";
+    }
+}
